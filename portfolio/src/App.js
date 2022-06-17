ca5247ed37f1a6272fea4fed3aa824ed497dabd8
Magnus Pladsen <93226629+MagnusPladsen@users.noreply.github.com>
2022-06-17 09:04:45 +0200

    Update to the hero image
    - Added so the background changes accordingly to the weather

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -5,11 +5,25 @@ import InspoQuote from './Components/InspoQuote';
 import Weather from './Components/Weather';
 import Skills from './Components/Skills';
 import Interests from './Components/Interests';
+import { useSelector } from 'react-redux';
+import { selectWeather } from './Features/weather';
 
 function App() {
+  const weather = useSelector(selectWeather); //Selects the weather from the store
+  const weatherBG = () => { //Returns the correct background for the weather condition from API
+    if (weather === 'Clear') {
+        return 'sunBG'
+    } else if (weather === 'Clouds') {
+        return 'cloudsBG'
+    } else if (weather === 'Rain') {
+        return 'rainBG'
+    } else if (weather === 'Snow') {
+        return 'snowBG'
+    }
+}
   return (
     <div className="App">
-      <div className="hero-image"></div>
+      <div className="hero-image" id={weatherBG()}></div>
       <Weather />
       <header className="App-header">
         <div className="grid-container">
