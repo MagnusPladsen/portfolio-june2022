57c40f7a15e85e8ad916405e6c62007684359744
Magnus Pladsen <93226629+MagnusPladsen@users.noreply.github.com>
2022-06-17 10:01:39 +0200

    Testing new function
    - Added show/hide button to skills and interest
    - Trying to make the default landing page more clean and show of more functionality

--- portfolio/src/Components/Interests.js
+++ portfolio/src/Components/Interests.js
@@ -1,16 +1,32 @@
 import React from 'react';
+import { useState } from 'react';
 
 const interests = [
-'Coding', 'Music', 'Movies', 'Gaming', 'Music Production', 'Computers'
+'Coding', 'Music', 'Movies', 'Gaming', 'FL Studio', 'Computers',
 ]
 
 export default function Interests() {
+    const [show, setShow] = useState('Show');
+    const [hide, setHide] = useState('hide');
+
+    const handleShowInterests = (e) => {
+        e.preventDefault();
+        if (hide === 'hide') {
+            setHide('show');
+            setShow('Hide');
+        } else {
+            setHide('hide');
+            setShow('Show');
+        }
+    }
+
     return (
         <div className="grid-item">
             <h2>Interests</h2>
-            <div className="inner-container">
+            <a href="#" className="cta cta-show" onClick={handleShowInterests}>{show}</a>
+            <div className="inner-container interests">
                 {interests.map((interest, index) => (
-                    <div className="inner-tile" key={index}>{interest}
+                    <div className="inner-tile" id={hide} key={index}>{interest}
                     </div>
             ))}
             </div>
--- portfolio/src/Components/Skills.js
+++ portfolio/src/Components/Skills.js
@@ -1,16 +1,31 @@
 import React from 'react';
+import { useState } from 'react';
 
 const skills = [
     'HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Adobe XD'
 ]
 
 export default function Skills() {
+    const [show, setShow] = useState('Show');
+    const [hide, setHide] = useState('hide');
+
+    const handleShowInterests = (e) => {
+        e.preventDefault();
+        if (hide === 'hide') {
+            setHide('show');
+            setShow('Hide');
+        } else {
+            setHide('hide');
+            setShow('Show');
+        }
+    }
     return (
         <div className="grid-item">
             <h2>Skills</h2>
+            <a href="#" className="cta cta-show" onClick={handleShowInterests}>{show}</a>
             <div className="inner-container">
                 {skills.map((skill, index) => (
-                    <div className="inner-tile" key={index}>{skill}
+                    <div className="inner-tile" id={hide} key={index}>{skill}
                     </div>
             ))}
             </div>
